878d44bda4b1a5c1349cf1adab7a2e1b8f5cc14e
Monsieur Zed <thibault.feuvrier@hotmail.fr>
2024-11-23 00:18:54 +0100

    fix

--- conf.ps1
+++ conf.ps1
@@ -1,4 +1,4 @@
-$debug = if (Test-Path "$env:TEMP\zedstoolkit\debug") { [bool](Get-Content -Path "$env:TEMP\zedstoolkit\debug" -Raw) } else { $false }
+$debug = $true
 
 $version = "v0.2.00"
 $base_path = if ($debug) { "D:\ZMT" } else { "https://raw.githubusercontent.com/MonsieurZed/ZTK/refs/heads/main/" }
