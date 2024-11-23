146d7d5a3d45988cd337ba5262ab21e86eac75d9
Monsieur Zed <thibault.feuvrier@hotmail.fr>
2024-11-23 02:14:35 +0100

    Update drawing.ps1

--- library/drawing.ps1
+++ library/drawing.ps1
@@ -110,7 +110,7 @@ function Get_Checkbox {
 
 function Draw_Applications {
     param (
-        [Json]$json,
+        [object]$json,
         [System.Windows.Controls.Panel]$wrap_panel,
         [System.Object]$source
     )
@@ -168,62 +168,62 @@ function Draw_Applications {
 }
 
 
-function Draw_Extensions {
-    param (
-        [string[]]$json_path,
-        [System.Windows.Controls.Panel]$wrap_panel
-    )
+# function Draw_Extensions {
+#     param (
+#         [string[]]$json_path,
+#         [System.Windows.Controls.Panel]$wrap_panel
+#     )
 
-    $json = Get-Content -Path $json_path -Raw | ConvertFrom-Json
-    $checkboxes = New-Object System.Collections.ArrayList
+#     $json = Get-Content -Path $json_path -Raw | ConvertFrom-Json
+#     $checkboxes = New-Object System.Collections.ArrayList
 
-    foreach ($app in $json.list) {
-        foreach ($browser in $app.browser) {
+#     foreach ($app in $json.list) {
+#         foreach ($browser in $app.browser) {
             
-            $items = $null
-            [void]($items = $app.items | Sort-Object -Property name)
+#             $items = $null
+#             [void]($items = $app.items | Sort-Object -Property name)
 
-            $groupBorder = Get_Group_Border
-            $groupPanel = Get_Group_Panel
+#             $groupBorder = Get_Group_Border
+#             $groupPanel = Get_Group_Panel
 
-            $title = if ($browser.name -eq "Main") { $app.title }else { $browser.name }
-            $header = GetTextblockHeader -text  $title -iconUrl $browser.icon
+#             $title = if ($browser.name -eq "Main") { $app.title }else { $browser.name }
+#             $header = GetTextblockHeader -text  $title -iconUrl $browser.icon
 
-            [void]$groupPanel.Children.Add($header)
+#             [void]$groupPanel.Children.Add($header)
     
-            foreach ($item in $items) {
+#             foreach ($item in $items) {
 
-                $itemPanel = Get_Item_Panel
+#                 $itemPanel = Get_Item_Panel
 
-                if ($item.icon) {
-                    try {
-                        $icon = Get_Icon_Panel -iconUrl $item.icon
-                        [void]$itemPanel.Children.Add($icon)
-                    }
-                    catch {
-                        Write-Error "Failed to load icon for $($item.name)" 
-                    }
-                }
+#                 if ($item.icon) {
+#                     try {
+#                         $icon = Get_Icon_Panel -iconUrl $item.icon
+#                         [void]$itemPanel.Children.Add($icon)
+#                     }
+#                     catch {
+#                         Write-Error "Failed to load icon for $($item.name)" 
+#                     }
+#                 }
 
-                $checkbox = Get_Checkbox -name $item.name -description $item.description
+#                 $checkbox = Get_Checkbox -name $item.name -description $item.description
                
-                $item | Add-Member -MemberType NoteProperty -Name "checkbox" -Value $checkbox -Force
-                $item | Add-Member -MemberType NoteProperty -Name "browser" -Value $browser.name -Force
+#                 $item | Add-Member -MemberType NoteProperty -Name "checkbox" -Value $checkbox -Force
+#                 $item | Add-Member -MemberType NoteProperty -Name "browser" -Value $browser.name -Force
 
-                [void]$checkboxes.Add($item)
+#                 [void]$checkboxes.Add($item)
 
-                [void]$itemPanel.Children.Add($checkbox)
+#                 [void]$itemPanel.Children.Add($checkbox)
             
-                [void]$groupPanel.Children.Add($itemPanel)
-            }
+#                 [void]$groupPanel.Children.Add($itemPanel)
+#             }
 
-            $groupBorder.Child = $groupPanel
+#             $groupBorder.Child = $groupPanel
 
-            [void]$wrap_panel.Children.Add($groupBorder)
-        }
-    }
-    return $checkboxes
-}
+#             [void]$wrap_panel.Children.Add($groupBorder)
+#         }
+#     }
+#     return $checkboxes
+# }
 
 
 function Draw_Buttons {
@@ -270,7 +270,7 @@ function Draw_Buttons {
 
 function Draw_Package {
     param (
-        [string[]]$json_path,
+        [object]$json,
         [System.Windows.Controls.ComboBox]$combo_box
     )
     $json = Get-Content -Path $json_path -Raw | ConvertFrom-Json
