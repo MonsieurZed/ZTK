c597d23acf44b21f73e0b95b4d2cfc6634a0a1f7
Monsieur Zed <thibault.feuvrier@hotmail.fr>
2024-11-29 11:00:46 +0100

    Squashed commit of the following:
    commit 3be7af3fc288fa8c30b72a9da1df447b8dd33473
    Author: Monsieur Zed <thibault.feuvrier@hotmail.fr>
    Date:   Fri Nov 29 11:00:22 2024 +0100
    
        fix
    
    commit e5cef172966e62fc29b03a586a9b3dcaa723205a
    Author: Monsieur Zed <thibault.feuvrier@hotmail.fr>
    Date:   Thu Nov 28 21:41:25 2024 +0100
    
        version
    
    commit 0d8238b8b53eb99c094466d8e9a7e053c9f415b9
    Author: Monsieur Zed <thibault.feuvrier@hotmail.fr>
    Date:   Thu Nov 28 21:39:40 2024 +0100
    
        colors

--- entry.ps1
+++ entry.ps1
@@ -14,8 +14,8 @@ if (Test-Path "$env:TEMP\zedstoolkit\debug") {
   $json_debug = Get-Content -Path "$env:TEMP\zedstoolkit\debug" -Raw | ConvertFrom-Json
   $Global:debug = $json_debug.debug
   $Global:base_path = $json_debug.path
-  . "$($Global:base_path)/conf.ps1"
   $Global:branch = "debug"
+  . "$($Global:base_path)/conf.ps1"
 } 
 else {
   $Global:branch = if ($MyInvocation.MyCommand -match "dev") { "dev" }else { "main" }
