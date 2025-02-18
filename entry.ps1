30c8fc460d6cbd2a6fb22ea58c59d7944d78c34c
Monsieur Zed <thibault.feuvrier@hotmail.fr>
2025-02-18 09:57:47 +0100

    fix
    entry script

--- entry.ps1
+++ entry.ps1
@@ -14,11 +14,14 @@ if (Test-Path "$env:TEMP\zedstoolkit\local.json") {
   $Global:local_json = Get-Content -Path "$env:TEMP\zedstoolkit\local.json" -Raw | ConvertFrom-Json
   $Global:debug = $local_json.debug
   $Global:base_path = $local_json.path
-  $Global:branch = "debug"
-  . "$($Global:base_path)/conf.ps1"
-} 
-else {
-  $Global:branch = if ($MyInvocation.MyCommand -match "dev") { "dev" }else { "main" }
-  $Global:base_path = "https://raw.githubusercontent.com/MonsieurZed/ZTK/refs/heads/$branch"
-  Invoke-Expression (Invoke-WebRequest -Uri "$($Global:base_path)/conf.ps1").Content
+  if ($Global:debug -eq $true) {
+    $Global:branch = "local_debug"
+    . "$($Global:base_path)/conf.ps1"
+    return
+  }
 }
+
+$Global:branch = if ($MyInvocation.MyCommand -match "dev") { "dev" }else { "main" }
+$Global:base_path = "https://raw.githubusercontent.com/MonsieurZed/ZTK/refs/heads/$branch"
+Invoke-Expression (Invoke-WebRequest -Uri "$($Global:base_path)/conf.ps1").Content
+
